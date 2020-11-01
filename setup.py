12d36fdd1f63087f793bc7646f8bccbd49677d53
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-11-01 17:14:57 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     DESCRIPTION = f.read()
 
 setup(name="paperetl",
-      version="1.2.0",
+      version="1.3.0",
       author="NeuML",
       description="ETL processes for medical and scientific papers",
       long_description=DESCRIPTION,
